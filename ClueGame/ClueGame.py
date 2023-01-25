7c43c3c247347bc3171ae97df57cf9f32cad9cb5
Nathan Mardanov <S1657228@online.houstonisd.org>
2023-01-25 11:38:43 -0600

    Full game, may need bug fixing

--- ClueGame/ClueGame.py
+++ ClueGame/ClueGame.py
@@ -44,10 +44,71 @@ def generateGame():
     
     return whodunnit
 
+culprit = generateGame()
+
 #Function to select player actions and respond accordingly
-def playerTurn(action: int, player: Player):
+def playerTurn(action: int, player, playerList: list):
+
+    #Move to a new room
     if action == 1:
-        player.room = player.note.pickRoom
+        player.room = player.note.pickRoom()
+        print(f"You moved to {player.room}. ")
+        if player.room == culprit['room']:
+            print(f"You deduced that the murder happened in {player.room}! Added to your notebook.")
+        else:
+            print(f"You can't find any evidence that the murder happened in {player.room}. Added to your notebook.")
+    
+    #Interrogate another player to find out if they are the culprit
+    elif action == 2:
+        suspect = player.note.interrogatePlayer()
+        print(f"You decided to interrogate {suspect}.")
+        if suspect == culprit['suspect']:
+            print(f"You deduced that the culprit was {suspect}! Added to your notebook.")
+        else:
+            print(f"You decided that {suspect} is innocent. Added to your notebook.")
+
+    #Inspect a weapon to determine if it was used in the murder
+    elif action == 3:
+        weapon = player.note.inspectWeapon()
+        print(f"You decided to inspect the {weapon}.")
+        if weapon == culprit['weapon']:
+            print(f"You deduced that the weapon used in the murder was the {weapon}! Added to your notebook.")
+        else:
+            print(f"You decided that the {weapon} was not used in the murder. Added to your notebook.")
+
+    #Check a player's notepad
+    elif action == 4:
+        print(player.note)
+
+    #Roll to succesfully print out (steal) another player's notepad
+    elif action == 5:
+        steal = int(input("Which player would you like to pickpocket?"))
+        if random.getrandbits(1):
+            print(f"You stole a notebook from player {steal}:\n")
+            print(playerList[steal-1].note)
+        else:
+            print(f"You were caught trying to pickpocket player {steal}!")
+
+    #Guess the culprit
+    elif action == 6:
+        who = input("WHO IS THE CULPRIT?")
+        how = input("WHAT WAS THE MURDER WEAPON?")
+        where = input("WHERE WAS THE CRIME COMMITTED?")
+        guess = {
+            'suspect' : who,
+            'weapon' : how,
+            'room' : where
+            }
+        if guess == culprit:
+            print("You won! The game is now over.")
+            return 1
+        else: 
+            num = 0
+            for i in culprit.keys():
+                if culprit[i] == guess[i]:
+                    num+=1
+            print(f"You guessed {num}/3 correct. Guess again.")
+
     return
 
 #Notepad class, to hold information on suspects for players
@@ -56,11 +117,46 @@ class Notepad:
         self.sus = sus
         self.wea = wea
         self.roo = roo
+        self.correct = []
+        self.wrong = []
 
     #Randomly select a room, and remove it from the list so rooms cannot be randomly selected twice. Used when players search a room to add it to the notepad
-    def pickRoom():
+    def pickRoom(self):
         random.shuffle(self.roo)
+        if self.roo[-1] == culprit['room']:
+            self.correct.append(self.roo[-1])
+        else:
+            self.wrong.append(self.roo[-1])
         return self.roo.pop()
+
+    #Select a random player, then check if they are the culprit and move them accordingly
+    def interrogatePlayer(self):
+        random.shuffle(self.sus)
+        if self.sus[-1] == culprit['suspect']:
+            self.correct.append(self.sus[-1])
+        else:
+            self.wrong.append(self.sus[-1])
+        return self.sus.pop()
+
+    def inspectWeapon(self):
+        random.shuffle(self.wea)
+        if self.wea[-1] == culprit['weapon']:
+            self.correct.append(self.wea[-1])
+        else:
+            self.wrong.append(self.wea[-1])
+        return self.wea.pop()
+
+    def __str__(self):
+        return f"""
+        CORRECT:
+        {self.correct}
+        INCORRECT:
+        {self.wrong}
+        UNKNOWN:
+        {self.sus}
+        {self.wea}
+        {self.roo}
+        """
 
 #Player class, holds info on notepad, name, past actions
 class Player:
@@ -74,7 +170,6 @@ class Player:
 
 #Main function to play the game
 def playGame():
-    culprit = generateGame()
 
     start = int(input("""
     Welcome to CLUE! Options:
@@ -95,23 +190,23 @@ def playGame():
     players = []
 
     for i in range(pnum):
-        players.append(Player(i, Notepad(), "Lobby"))
+        players.append(Player(i, Notepad(suspects, weapons, rooms), "Lobby"))
 
     #Main loop for each player's turn during the game
     while True:
         for p in players:
-            move = input(f"""
-            Player {p.num}, select an action:
+            move = int(input(f"""
+            Player {p.num + 1}, select an action:
             1 - Move to (and search) a new room
-            2 - Interrogate
-            3 - Check notepad
-            4 - Steal a notepad
-            5 - Guess the culprit
-            """)
-
-
-
-
+            2 - Interrogate a suspect
+            3 - Inspect a weapon
+            4 - Check notepad
+            5 - Steal a notepad
+            6 - Guess the culprit
+            """))
+            if playerTurn(move, p, players) == 1:
+                culprit = generateGame()
+                return
 
 
 #TESTING
